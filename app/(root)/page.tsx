a4af7eed95414d71baa72a8b7e0121403d2be29a
Partha807222 <part22034.cb@rmkec.ac.in>
2025-08-22 10:18:27 +0530

    implement insial file

---
+++ app/(root)/page.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const HOME = () => {
+  return (
+    <section className="home">
+      <div className="home—content">
+        <header>
+          Welcome, Adrian
+          </header>
+      </div>
+    </section>
+  )
+}
+
+export default HOME
+
+
+
